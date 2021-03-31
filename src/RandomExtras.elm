10674d17198228588073ec9edc4f64800a17eff7
rerades <rerades@siete3.com>
2021-03-31 07:23:33 +0200

    working random

--- src/RandomExtras.elm
+++ src/RandomExtras.elm
@@ -1,8 +1,12 @@
-module RandomExtras exposing (..)
+module RandomExtras exposing (byRange,byRangeInt)
 
 import Random exposing (Seed)
 
 byRange: Seed -> (Float, Float) -> Float
 byRange seed (from,to) =
     Random.step (Random.float from to) seed
+        |> Tuple.first
+
+byRangeInt seed (from,to) =
+    Random.step (Random.int from to) seed
         |> Tuple.first
